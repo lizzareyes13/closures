0380c7d389e1c57ff21fce051512da83bbde1bc0
Lizza Reyes <Lizzareyes7@gmail.com>
2017-09-25 19:40:47 -0400

    Create closures.js

---
+++ closures.js
@@ -0,0 +1,12 @@
+//challenge 2
+
+function createFunctionPrinter() {
+  var printSample = "sample";
+  var printHello = "Hello";
+  function createdFunc() {
+    printHello;
+  }
+  return printHello;
+}
+
+createFunctionPrinter();
