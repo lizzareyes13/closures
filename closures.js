2c4ba65bf5a08689e15594c65e21efe356fc701c
Lizza Reyes <Lizzareyes7@gmail.com>
2017-09-25 22:15:28 -0400

    Update closures.js

--- closures.js
+++ closures.js
@@ -7,3 +7,47 @@ function createFunction() {
   
   return result;
 }
+
+//challenge 2
+
+var printSample = 'sample';
+function createFunctionPrinter('sample') {
+  var printHello = 'Hello';
+  function createdFunc() {
+    printHello
+  }
+  return printHello;
+}
+
+console.log();
+
+
+//challenge 4
+
+function addByX(x) {
+  return function add(num) {
+    return x + num;
+  };
+}
+
+var addByTwo = addByX(2);
+var result = addByTwo(1);
+console.log(result);
+
+//challenge 5
+
+function once(func) {
+  var result;
+  return function() {
+    if(func) {
+      result = func.apply(null, arguments);
+      func = null;
+    }
+    return result;
+  }
+}
+
+var onceFunc = once(addByTwo);
+
+console.log(onceFunc(4));
+
